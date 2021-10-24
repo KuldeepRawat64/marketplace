4e95d61e6797d97597c86ab7260d118cf7937bce
Nilesh <nilesh@oxloop.com>
2021-10-24 17:51:56 +0530

    - project connected with firebase - Google sign in and email sin in implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
-import 'package:demo/home.dart';
+import 'package:demo/pages/home_page.dart';
+import 'package:demo/provider/email_sign_in.dart';
+import 'package:demo/provider/google_sign_in.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  static final String title = 'Google SignIn';
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Login',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Home(),
-    );
-  }
+  Widget build(BuildContext context) => MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => GoogleSignInProvider()),
+          ChangeNotifierProvider(create: (context) => EmailSignInProvider()),
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: title,
+          theme: ThemeData(primarySwatch: Colors.deepOrange),
+          home: HomePage(),
+        ),
+      );
 }
